e8b66cf48f1e49e33eaa6c7a5bce43377679a1af
Serraji Mohssine <sera.mohssine@gmail.com>
2021-02-16 02:54:49 +0100

    login

--- src/app/pages/register/register-routing.module.ts
+++ src/app/pages/register/register-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { FirstPage } from './first.page';
+import { RegisterPage } from './register.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: FirstPage
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
+    component: RegisterPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class FirstPageRoutingModule {}
+export class RegisterPageRoutingModule {}
