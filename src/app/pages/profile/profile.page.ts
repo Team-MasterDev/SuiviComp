7ba1c1400d219b2c2c12c29f4842e77dffac61db
Serraji Mohssine <sera.mohssine@gmail.com>
2021-02-22 23:31:18 +0100

    profile with change and without change

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  title = 'Account';
+
+  label = 'User name';
+  value = 'James Warden';
+  change = 'tabs';
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  log(){
+    console.log('momo');
+  }
+
+
 
 }
